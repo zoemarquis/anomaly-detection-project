61e3fff8b9b8ee5d8b8a8c45a79c5eefc15dc6bf
Zoé Marquis <zoe.marquis.zm@gmail.com>
2024-10-29 13:59:37 +0100

    premier streamlit

---
+++ streamlit_zoe_bar_chart_nb_labels_per_dataset.py
@@ -0,0 +1,54 @@
+from streamlit_defaults import *
+
+
+# --- Visualisation ---
+
+# -----------------------------------------------------------------------------------------
+# Bar chart : nb labels per dataset
+
+st.title("Physical dataset")
+st.header("Number of labels per dataset")
+
+    
+
+# Créer une liste de tous les DataFrames et noms pour les identifier
+datasets = [
+    ("Attack 1", df_phy_1),
+    ("Attack 2", df_phy_2),
+    ("Attack 3", df_phy_3),
+    ("Attack 4", df_phy_4),
+    ("Normal", df_phy_norm)
+]
+
+# Préparation des données pour l'histogramme empilé
+data = []
+unique_labels = set()
+
+# Extraction des informations de chaque dataset
+for name, df in datasets:
+    label_counts = df["Label"].value_counts()
+    unique_labels.update(label_counts.index)  # Recueillir tous les labels uniques
+    for label, count in label_counts.items():
+        data.append({
+            "Dataset": name,
+            "Label": label,
+            "Count": count
+        })
+
+# Convertir en DataFrame et associer des couleurs aux labels uniques
+data_df = pd.DataFrame(data)
+color_discrete_map = {label: color for label, color in zip(unique_labels, default_colors)}
+print(label_color_map)
+
+# Créer l'histogramme empilé
+fig = px.bar(
+    data_df,
+    x="Dataset",
+    y="Count",
+    color="Label",
+    color_discrete_map=label_color_map,
+    title="Répartition des labels par dataset",
+    labels={"Count": "Nombre d'étiquettes"}
+)
+
+st.plotly_chart(fig)
