f61b0a3dd9163a16559bbb367fefc0dd6e7abe00
charlotte <117921625+charlottekruzic@users.noreply.github.com>
2024-11-12 17:35:58 +0100

    Ajout distribution par labels

--- pages/exploration_données_network.py
+++ pages/exploration_données_network.py
@@ -29,6 +29,11 @@ def load_data():
         st.error(f"Erreur lors du chargement des données : {e}")
         return {}
 
+@st.cache_resource
+def combine_data(dataframes):
+    combined_df = pd.concat([df.assign(source=name) for name, df in dataframes.items()], ignore_index=True)
+    return combined_df
+
 
 
 dataframes = load_data()
@@ -53,7 +58,7 @@ st.write("Comparaison du Nombre de Valeurs Uniques par Colonne")
 st.dataframe(unique_values_df)
 
 # Partie 2
-st.header("Comparaison des Distributions")
+st.header("Comparaison des distributions entre les fichiers")
 
 # A
 st.subheader("Répartition des labels")
@@ -145,16 +150,16 @@ st.plotly_chart(fig_modbus_fn)
 st.subheader("Distribution des réponses Modbus")
 fig_modbus_response = go.Figure()
 for name, df in dataframes.items():
-    top_modbus_response = df['modbus_response'].value_counts().nlargest(10)
+    top_modbus_response = df['modbus_response'].value_counts()
     fig_modbus_response.add_trace(go.Bar(x=top_modbus_response.index, y=top_modbus_response.values, name=name))
-fig_modbus_response.update_layout(title="Distribution des réponses Modbus (Top 10)", xaxis_title="Réponse Modbus", yaxis_title="Fréquence", barmode="stack")
+fig_modbus_response.update_layout(title="Distribution des réponses Modbus", xaxis_title="Réponse Modbus", yaxis_title="Fréquence", barmode="stack")
 st.plotly_chart(fig_modbus_response)
 
 # I
 st.subheader("Distribution du nombre de paquets")
 fig_n_pkt_src = go.Figure()
 for name, df in dataframes.items():
-    top_n_pkt_src = df['n_pkt_src'].value_counts().nlargest(10)
+    top_n_pkt_src = df['n_pkt_src'].value_counts()
     fig_n_pkt_src.add_trace(go.Bar(x=top_n_pkt_src.index, y=top_n_pkt_src.values, name=name))
 fig_n_pkt_src.update_layout(title="Distribution du nombre de paquets source", xaxis_title="Nombre de paquets source", yaxis_title="Fréquence", barmode="stack")
 st.plotly_chart(fig_n_pkt_src)
@@ -165,4 +170,114 @@ for name, df in dataframes.items():
     top_n_pkt_dst = df['n_pkt_dst'].value_counts()
     fig_n_pkt_dst.add_trace(go.Bar(x=top_n_pkt_dst.index, y=top_n_pkt_dst.values, name=name))
 fig_n_pkt_dst.update_layout(title="Distribution du nombre de paquets destination", xaxis_title="Nombre de paquets destination", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_n_pkt_dst)
+
+
+# Partie 3
+combined_df = combine_data(dataframes)
+
+st.header("Comparaison des distributions entre les labels")
+
+# A
+st.subheader("Distribution des ports par label")
+fig_sport = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_sport = label_df['sport'].value_counts()
+    fig_sport.add_trace(go.Bar(x=top_sport.index, y=top_sport.values, name=label))
+
+fig_sport.update_layout(title="Distribution des ports source par label", xaxis_title="Port source", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_sport)
+
+fig_dport = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_dport = label_df['dport'].value_counts()
+    fig_dport.add_trace(go.Bar(x=top_dport.index, y=top_dport.values, name=label))
+
+fig_dport.update_layout(title="Distribution des ports destination par label", xaxis_title="Port destination", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_dport)
+
+# B
+st.subheader("Distribution des flags TCP par label")
+fig_flags = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_flags = label_df['flags'].value_counts()
+    fig_flags.add_trace(go.Bar(x=top_flags.index, y=top_flags.values, name=label))
+
+fig_flags.update_layout(title="Distribution des flags TCP par label", xaxis_title="Flags TCP", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_flags)
+
+# C
+st.subheader("Distribution de la taille des paquets par label")
+fig_size = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_size = label_df['size'].value_counts()
+    fig_size.add_trace(go.Bar(x=top_size.index, y=top_size.values, name=label))
+
+fig_size.update_layout(title="Distribution de la taille des paquets par label", xaxis_title="Taille des paquets", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_size)
+
+# D
+st.subheader("Distribution des adresses IP par label")
+fig_ip_s = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_ip_s = label_df['ip_s'].value_counts()
+    fig_ip_s.add_trace(go.Bar(x=top_ip_s.index, y=top_ip_s.values, name=label))
+
+fig_ip_s.update_layout(title="Distribution des adresses IP source par label", xaxis_title="Adresse IP source", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_ip_s)
+
+fig_ip_d = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_ip_d = label_df['ip_d'].value_counts()
+    fig_ip_d.add_trace(go.Bar(x=top_ip_d.index, y=top_ip_d.values, name=label))
+
+fig_ip_d.update_layout(title="Distribution des adresses IP destination par label", xaxis_title="Adresse IP destination", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_ip_d)
+
+# E
+st.subheader("Distribution des fonctions Modbus par label")
+fig_modbus_fn = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_modbus_fn = label_df['modbus_fn'].value_counts()
+    fig_modbus_fn.add_trace(go.Bar(x=top_modbus_fn.index, y=top_modbus_fn.values, name=label))
+
+fig_modbus_fn.update_layout(title="Distribution des fonctions Modbus par label", xaxis_title="Fonction Modbus", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_modbus_fn)
+
+# F
+st.subheader("Distribution des réponses Modbus par label")
+fig_modbus_response = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_modbus_response = label_df['modbus_response'].value_counts()
+    fig_modbus_response.add_trace(go.Bar(x=top_modbus_response.index, y=top_modbus_response.values, name=label))
+
+fig_modbus_response.update_layout(title="Distribution des réponses Modbus par label", xaxis_title="Réponse Modbus", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_modbus_response)
+
+# G
+st.subheader("Distribution du nombre de paquets par label")
+fig_n_pkt_src = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_n_pkt_src = label_df['n_pkt_src'].value_counts()
+    fig_n_pkt_src.add_trace(go.Bar(x=top_n_pkt_src.index, y=top_n_pkt_src.values, name=label))
+
+fig_n_pkt_src.update_layout(title="Distribution du nombre de paquets source par label", xaxis_title="Nombre de paquets source", yaxis_title="Fréquence", barmode="stack")
+st.plotly_chart(fig_n_pkt_src)
+
+fig_n_pkt_dst = go.Figure()
+for label in combined_df['label'].unique():
+    label_df = combined_df[combined_df['label'] == label]
+    top_n_pkt_dst = label_df['n_pkt_dst'].value_counts()
+    fig_n_pkt_dst.add_trace(go.Bar(x=top_n_pkt_dst.index, y=top_n_pkt_dst.values, name=label))
+
+fig_n_pkt_dst.update_layout(title="Distribution du nombre de paquets destination par label", xaxis_title="Nombre de paquets destination", yaxis_title="Fréquence", barmode="stack")
 st.plotly_chart(fig_n_pkt_dst)
